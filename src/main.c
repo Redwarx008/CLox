5eb331bef35c55b9aa7ebe20cd66ec44c164cdbc
Redwarx008 <redwarx008@outlook.com>
2023-06-15 18:18:25 +0800

    a virtual machine

--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
 ﻿#include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main()
 {
+	initVM();
 	Chunk chunk;
 	initChunk(&chunk);
 	int constant = addConstant(&chunk, 1.2);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
+	writeChunk(&chunk, OP_NEGATE, 123);
 	writeChunk(&chunk, OP_RETURN, 123);
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&chunk);
+	freeVM();
 	freeChunk(&chunk);
 	return 0;
 }
