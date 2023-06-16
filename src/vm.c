a2b4e558839fa8b4b6ddaa123aebe6fe65995e7c
Redwarx008 <redwarx008@outlook.com>
2023-06-16 18:16:29 +0800

    scan -1

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "common.h"
 #include "vm.h"
+#include "compiler.h"
 #include "debug.h"
 
 VM vm;
@@ -29,6 +30,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
 
@@ -50,6 +57,10 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/ ); break;
             case OP_NEGATE:   push(-pop()); break;
 
             case OP_RETURN: {
@@ -62,11 +73,12 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 
-InterpretResult interpret(Chunk* chunk) {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
+
 }
